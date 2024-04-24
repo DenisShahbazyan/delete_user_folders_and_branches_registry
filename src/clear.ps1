8721d07dbf24f38e7a2e1f36c2a88cdb94e6589c
Денис Шахбазян <89923877+DenisShahbazyan@users.noreply.github.com>
2024-04-24 16:24:53 +0300

    Create test.ps1
    Update test.ps1
    
    Update test.ps1

---
+++ src/clear.ps1
@@ -0,0 +1,94 @@
+﻿$width = 120
+$height = 30
+$size = New-Object System.Management.Automation.Host.Size($width, $height)
+$host.ui.rawui.windowsize = $size
+
+# Проверка на администратора
+$isAdmin = ([System.Security.Principal.WindowsIdentity]::GetCurrent()).groups -match "S-1-5-32-544"
+if (-not $isAdmin) {
+    Start-Process powershell -ArgumentList " -NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs
+    exit
+}
+
+
+$excludedNames = @(
+    "Default", 
+    "Public", 
+    "Общие", 
+    "Default User", 
+    "Пользователь по умолчанию", 
+    "Administrator", 
+    "Администратор", 
+    "dezhur112", 
+    "defaultuser", 
+    "defaultuser0", 
+    "admin112", 
+    "user", 
+    "secure", 
+    "systemprofile", 
+    "LocalService", 
+    "NetworkService", 
+    $env:USERNAME
+).ToLower()
+$usersFolder = "C:\Users"
+$allUserFolders = Get-ChildItem -Path $usersFolder | Where-Object {
+    $_.PSIsContainer -and $excludedNames -notcontains $_.Name.ToLower()
+}
+
+
+# Удаление профилей на диске
+$logicalProcessors = (Get-CimInstance -ClassName Win32_Processor).NumberOfCores
+
+Write-Host "УДАЛЕНИЕ ПОЛЬЗОВАТЕЛЬСКИХ ПАПОК"
+$allUserFolders | ForEach-Object -Parallel {
+    Write-Progress -Activity "Удаление папки профиля: $_" -Status "Удаление..."
+    $path = $_.FullName
+    try {
+        Remove-Item -Path $path -Recurse -Force -ErrorAction Stop
+        Write-Host "--- $(Get-Date -Format 'HH:mm:ss') - Папка профиля $_ удалена." -ForegroundColor Green
+    }
+    catch {
+        # Если возникла ошибка, пробую изменить разрешения
+        $_.Exception | ForEach-Object {
+            $failedItem = $_.TargetObject
+            if ($failedItem) {
+                # Попытка взять владение и изменить разрешения
+                takeown.exe /F $failedItem /A /R /D Y
+                icacls $failedItem /reset /T /C /Q
+                icacls $failedItem /setowner "Administrators" /T /C /Q
+                icacls $failedItem /grant "Administrators":(F) /T /C /Q
+                # Попытка удалить ещё раз
+                Remove-Item -Path $failedItem -Recurse -Force -ErrorAction SilentlyContinue
+            }
+        }
+        # Проверка, удалена ли папка после изменения разрешений
+        if (-not (Test-Path -Path $path)) {
+            Write-Host "--- $(Get-Date -Format 'HH:mm:ss') - Папка профиля $_ удалена после изменения разрешений." -ForegroundColor Green
+        }
+        else {
+            Write-Host "--- $(Get-Date -Format 'HH:mm:ss') - Не удалось удалить папку профиля $_ даже после изменения разрешений." -ForegroundColor Red
+        }
+    }
+} -ThrottleLimit $logicalProcessors
+Write-Progress -Activity "Пользовательские папки профилей удалены!" -Completed
+
+
+# Удаление профилей в реестре
+$profileListPath = "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\ProfileList"
+$profileSubKeys = Get-ChildItem -Path $profileListPath
+
+Write-Host "УДАЛЕНИЕ ПОЛЬЗОВАТЕЛЬСКИХ ПРОФИЛЕЙ ИЗ РЕЕСТРА"
+foreach ($subKey in $profileSubKeys) {
+    $profileImagePath = (Get-ItemProperty -Path $subKey.PSPath).ProfileImagePath
+    $profileName = $profileImagePath -split '\\' | Select-Object -Last 1
+
+    if ($profileName -notin $excludedNames) {
+        Write-Progress -Activity "Удаление ветки профиля: $subKey.PSPath" -Status "Удаление..."
+        Remove-Item -Path $subKey.PSPath -Force -Recurse
+        Write-Host "--- Ветка профиля $profileName удалена." -ForegroundColor Green
+    }
+}
+Write-Progress -Activity "Пользовательские ветки профилей удалены из реестра" -Completed
+
+Write-Host "ГОТОВО."
+pause
---
+++ src/main.ps1
@@ -0,0 +1,66 @@
+﻿$installPath = "C:\Program Files\PowerShell"
+$powershellCorePath = Join-Path -Path $installPath -ChildPath "7\pwsh.exe"
+$minimumVersion = [System.Version]::new(7, 3, 10)
+$scriptDirectory = $MyInvocation.MyCommand.Path | Split-Path
+$scriptName = "clear.ps1"
+$clearScriptPath = Join-Path -Path $scriptDirectory -ChildPath $scriptName
+
+
+function Start-Administrator {
+    $isAdmin = ([System.Security.Principal.WindowsIdentity]::GetCurrent()).groups -match "S-1-5-32-544"
+    if (-not $isAdmin) {
+        Start-Process powershell -ArgumentList " -NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs
+        exit
+    }
+}
+
+# Установка PS Core 7.3.10
+function Install-PowerShellCore {
+    $installerUrl = "https://github.com/PowerShell/PowerShell/releases/download/v7.3.10/PowerShell-7.3.10-win-x64.msi"
+    $installerPath = "$env:TEMP\PowerShellInstaller.msi"
+    Invoke-WebRequest -Uri $installerUrl -OutFile $installerPath
+    Start-Process -Wait -FilePath msiexec.exe -ArgumentList "/i $installerPath /quiet ADD_EXPLORER_CONTEXT_MENU_OPENPOWERSHELL=1 ENABLE_PSREMOTING=1 REGISTER_MANIFEST=1"
+    [Environment]::SetEnvironmentVariable('Path', "$($env:Path);$installPath", [System.EnvironmentVariableTarget]::Machine)
+    Remove-Item -Path $installerPath -Force
+}
+
+# Проверка версии PowerShell Core
+function Get-PowerShellCoreVersion {
+    if (-not (Test-Path $powershellCorePath -PathType Leaf)) {
+        return $null
+    }
+    $versionInfo = Get-Item -LiteralPath $powershellCorePath | Get-ItemProperty | Select-Object -Property VersionInfo
+    $version = $versionInfo.VersionInfo.ProductVersion.Split(' ')[0]
+    $versionObject = [System.Version]$version
+    return $versionObject
+}
+
+function Start-CheckPowerShellCore {
+    $currentVersion = Get-PowerShellCoreVersion
+    
+    If ($currentVersion -lt $minimumVersion) {
+        Write-Host "PowerShell Core отсутствует или его версия ниже $minimumVersion."
+        Write-Progress -Activity "Запускаю установку PowerShell Core $minimumVersion." -Status "Установка..."
+        Install-PowerShellCore
+        Write-Progress -Activity "Установка PowerShell Core $(Get-PowerShellCoreVersion) завершена!" -Completed
+    }
+    else {
+        Write-Host "Установленная версия PowerShell Core $(Get-PowerShellCoreVersion)"
+    }
+}
+
+function Start-CleaningScript {
+    if (Test-Path $clearScriptPath -PathType Leaf) {
+        Write-Host "Запуск сценария очистки..." -ForegroundColor Green
+        Start-Process -FilePath $powershellCorePath -ArgumentList "-NoProfile -ExecutionPolicy Bypass -File `"$clearScriptPath`"" -Verb RunAs
+    }
+    else {
+        Write-Host "Скрипт для очистки не найден. Поместите скрипт с именем $scriptName рядом с этим скриптом." -ForegroundColor Red
+    }     
+}
+
+Start-Administrator
+Start-CheckPowerShellCore
+Start-CleaningScript
+
+pause
